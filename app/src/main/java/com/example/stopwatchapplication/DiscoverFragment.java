d44ae6b9e8b887e7681c28762b16900e2dc56a0e
Steve <stevel@csedge.org>
2020-05-02 20:20:58 -0700

    Added recycler

--- app/src/main/java/com/example/stopwatchapplication/DiscoverFragment.java
+++ app/src/main/java/com/example/stopwatchapplication/DiscoverFragment.java
@@ -1,16 +1,17 @@
 package com.example.stopwatchapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,14 +23,16 @@ import android.widget.Toast;
 
 import com.example.stopwatchapplication.R;
 
+import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DiscoverFragment extends Fragment {
-
     ImageButton playBtn;
     TextView remainingTimeLabel;
     MediaPlayer mp;
@@ -37,23 +40,36 @@ public class DiscoverFragment extends Fragment {
     TextView lucidDreams;
     TextView robbery;
 
+    ArrayList<Object> songList;
+
+    RecyclerAdapter adapter;
+
     public DiscoverFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        super.onCreate(savedInstanceState);
+        Log.d("onCreateView", "starting...");
+
+        addItemsToRecyclerViewArrayList();
+        // super.onCreate(savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_discover, container, false);
 
-        playBtn = (ImageButton) v.findViewById(R.id.playBtn);
-        remainingTimeLabel = (TextView) v.findViewById(R.id.remainingTimeLabel);
-        lucidDreams = (TextView) v.findViewById(R.id.lucidDreams);
-        robbery = (TextView) v.findViewById(R.id.robbery);
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+
+
+        RecyclerAdapter adapter = new RecyclerAdapter(songList);
+        recyclerView.setAdapter(adapter);
+
+
+        //playBtn = (ImageButton) v.findViewById(R.id.playBtn);
+        //remainingTimeLabel = (TextView) v.findViewById(R.id.remainingTimeLabel);
+        //lucidDreams = (TextView) v.findViewById(R.id.lucidDreams);
+        //robbery = (TextView) v.findViewById(R.id.robbery);
 
 
         // Media Player
@@ -64,19 +80,19 @@ public class DiscoverFragment extends Fragment {
         totalTime = mp.getDuration();
 
         // Media Player
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                if (!mp.isPlaying()) {
-                    mp.start();
-                    playBtn.setImageResource(R.drawable.pause);
-                } else {
-                    mp.pause();
-                    playBtn.setImageResource(R.drawable.ic_play);
-                }
-
-            }
-        });
+        //        playBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick (View v) {
+//                if (!mp.isPlaying()) {
+//                    mp.start();
+//                    playBtn.setImageResource(R.drawable.pause);
+//                } else {
+//                    mp.pause();
+//                    playBtn.setImageResource(R.drawable.ic_play);
+//                }
+//
+//            }
+//        });
 
         new Thread(new Runnable() {
             @Override
@@ -91,7 +107,6 @@ public class DiscoverFragment extends Fragment {
                 }
             }
         }).start();
-
         Typeface MLight = Typeface.createFromAsset(getContext().getAssets(), "fonts/MLight.ttf");
         Typeface MMedium = Typeface.createFromAsset(getContext().getAssets(), "fonts/MMedium.ttf");
         Typeface MRegular = Typeface.createFromAsset(getContext().getAssets(), "fonts/MRegular.ttf");
@@ -102,13 +117,13 @@ public class DiscoverFragment extends Fragment {
 
     private Handler handler = new Handler() {
         @Override
-        public void handleMessage(Message msg) {
+        public void handleMessage(@NotNull Message msg) {
             int currentPosition = msg.what;
 
             // Update Labels.
 
             String remainingTime = createTimeLabel(totalTime-currentPosition);
-            remainingTimeLabel.setText("- " + remainingTime);
+            //    remainingTimeLabel.setText("- " + remainingTime);
         }
     };
 
@@ -116,11 +131,31 @@ public class DiscoverFragment extends Fragment {
         String timeLabel = "";
         int min = time / 1000 / 60;
         int sec = time / 1000 % 60;
-
         timeLabel = min + ":";
         if (sec < 10) timeLabel += "0";
         timeLabel += sec;
 
         return timeLabel;
     }
+
+    // Function to add items in RecyclerView.
+    public void addItemsToRecyclerViewArrayList()
+    {
+        // Adding items to ArrayList
+        songList = new ArrayList<>();
+        songList.add("Song 1");
+        songList.add("Song 2");
+        songList.add("Song 3");
+        songList.add("Song 4");
+        songList.add("Song 5");
+        songList.add("Song 6");
+        songList.add("Song 7");
+        songList.add("Song 8");
+
+    }
 }
+
+
+
+
+
